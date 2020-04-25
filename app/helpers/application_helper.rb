c76696707a0049919bf4db4de99bf408bdf7c6eb
GitKraken <2302531309@qq.com>
2020-04-25 17:34:05 +0800

    FEAT: 添加顶部导航组件

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def authenticate_admin!
     redirect_to root_path unless current_user.admin?
   end
+
+  # 加载 VUE 组件的 template 内容
+  def import_partial(partial)
+    # 把 template 内容去除空白字符拼成一行
+    res = render(partial).split("\n").join('')
+    raw(res)
+  end
 end
