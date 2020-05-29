9456fc3c30eb10666b543dd980ec83bd2df9ae2a
GitKraken <2302531309@qq.com>
2020-05-29 22:35:28 +0800

    FEAT: 已有rss feed不会重复添加

--- app/models/rss_probe_history.rb
+++ app/models/rss_probe_history.rb
@@ -16,11 +16,13 @@ class RssProbeHistory < ApplicationRecord
                                link: link)
 
     items.each do |item|
-      rss_probe_history.rss_feeds.create(title: item[:title],
-                                         description: item[:description],
-                                         author: item[:author],
-                                         pub_date: item[:pub_date],
-                                         link: item[:link])
+      # 增量更新，已有的RSS Feed不会重复添加
+      RssFeed.create_with(title: item[:title],
+                          description: item[:description],
+                          author: item[:author],
+                          pub_date: item[:pub_date],
+                          rss_probe_history_id: rss_probe_history.id)
+          .find_or_create_by(link: item[:link])
     end
   end
 end
