118398cd9a1b892fcface219b727baa924e984a8
GitKraken <2302531309@qq.com>
2020-11-28 10:42:36 +0800

    STYLE: concern 代码调整

--- app/controllers/concerns/rss_setting_list_concern.rb
+++ app/controllers/concerns/rss_setting_list_concern.rb
@@ -4,13 +4,14 @@ module RSSSettingListConcern
   extend ActiveSupport::Concern
 
   included do |base|
-    def running_rss_jobs_count
-      Sidekiq::Queue.new('rss_job').size
-    end
+  end
 
-    def running_goods_jobs_count
-      Sidekiq::Queue.new('web_spider').size
-    end
+  def running_rss_jobs_count
+    Sidekiq::Queue.new('rss_job').size
+  end
+
+  def running_goods_jobs_count
+    Sidekiq::Queue.new('web_spider').size
   end
 
   module ClassMethods
