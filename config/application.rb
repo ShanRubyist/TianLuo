810d4c063adc52f5e99aa9e651cd2ff0c9b5ec86
GitKraken <2302531309@qq.com>
2021-05-02 19:53:34 +0800

    FEAT: 优化 meta 标签

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module TianLuo
     require 'server'
     config.middleware.use Server
 
+    config.middleware.use Rack::Deflater
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
