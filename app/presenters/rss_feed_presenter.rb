1d3990c09c2612f15e242769874d1df8005d9485
Shan <2302531309@qq.com>
2021-12-19 16:09:42 +0800

    FIX:修复bug

--- app/presenters/rss_feed_presenter.rb
+++ app/presenters/rss_feed_presenter.rb
@@ -4,23 +4,25 @@ class RssFeedPresenter < BasePresenter
   def rss_feed
     RssFeed.rss_feeds_list(@locals[:user_id], @locals[:rss], @locals[:page], @locals[:per]).map do |rss|
       {
-          id: rss.id,
-          title: rss.title,
-          description: Loofah.fragment(rss.description).scrub!(:escape).to_html,
-          pub_date: rss.pub_date.nil? ? '' : rss.pub_date.localtime.strftime("%Y-%m-%d %H:%M"),
-          author: rss.author,
-          link: rss.link,
-          rss: rss.probe_setting.rss_info.title,
-          rss_link: rss.probe_setting.rss_info.link,
-          rss_description: rss.probe_setting.rss_info.description,
-          icon: rss.probe_setting.rss_info.icon,
-          status: !rss.user_rss_feed_ships.first&.unread,
-          thumbs_up: rss.user_rss_feed_ships.first&.thumbs_up,
-          tags: {
+        id: rss.id,
+        title: rss.title,
+        description: Loofah.fragment(rss.description).scrub!(:escape).to_html,
+        pub_date: rss.pub_date.nil? ? '' : rss.pub_date.localtime.strftime("%Y-%m-%d %H:%M"),
+        author: rss.author,
+        link: rss.link,
+        rss: rss.probe_setting.rss_info.title,
+        rss_link: rss.probe_setting.rss_info.link,
+        rss_description: rss.probe_setting.rss_info.description,
+        icon: rss.probe_setting.rss_info.icon,
+        status: !rss.user_rss_feed_ships.first&.unread,
+        thumbs_up: rss.user_rss_feed_ships.first&.thumbs_up,
+        tags: rss.tags.map do |tag|
+          {
             name: tag.name,
             tf_idf: rss.rss_feed_tag_ships.find_by(tag: tag).tf_idf,
-            recommend: recommend_tags.map(&:tag_id).include?(tag.id)
+            recommend: ''
           }
+        end
       }
     end
   end
